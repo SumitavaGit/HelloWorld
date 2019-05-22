036aada98963e443d4a0a568f8db7a60d24c6423
biswasum <biswasum@WL354839.mshome.net>
2019-05-22 11:52:53 +0530

    https://github.com/SumitavaGit/HelloWorld/projects/1#card-21831114

--- SOAPWSDemo/src/main/java/com/service/ServicePublisher.java
+++ SOAPWSDemo/src/main/java/com/service/ServicePublisher.java
@@ -3,6 +3,7 @@ package com.service;
 import javax.xml.ws.Endpoint;
 
 public class ServicePublisher {
+	// This is a end point
 	public static void main(String[] agrs) {
 		Endpoint.publish("http://localhost:8080/SOAPWSDemo", new MyService());
 	}
